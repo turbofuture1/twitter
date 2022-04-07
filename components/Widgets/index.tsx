01e6fe0d28852d6fb9fccc675eb71056eae002a6
turbofuture1 <alimbadmus239@gmail.com>
2022-04-07 16:46:38 +0100

    final final commit

--- components/Widgets/index.tsx
+++ components/Widgets/index.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 const Widgets: NextPage<Props> = ({ trendingResults, followResults }) => {
   return (
-    <div className="ml-8 hidden space-y-5 py-1 lg:inline xl:w-[500px]">
+    <div className="ml-8 hidden space-y-5 py-1 lg:inline xl:w-[950px]">
       <div className="sticky top-0 z-50 w-11/12 bg-black py-1.5 xl:w-9/12">
         <div className="relative flex items-center rounded-full bg-[#202327] p-3">
           <SearchIcon className="z-50 h-5 text-gray-500" />
